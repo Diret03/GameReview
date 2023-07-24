1655ec1478fcebfe947d3aa1b3bfa480d9993da0
Diret03 <diegodavidrecalde@gmail.com>
2023-07-23 23:27:41 -0500

    correcciones
    bug al eliminar juego corregido
    nuevos juegos añadidos

--- main/deleteGame.php
+++ main/deleteGame.php
@@ -1,17 +1,26 @@
 <?php
 
+
 include '../php/db.php';
 
 if (isset($_GET['deleteid'])) {
 
     $gameID = $_GET['deleteid'];
 
-    $sql = "DELETE FROM games WHERE gameID=$gameID";
+    // Eliminar registros relacionados en la tabla 'review'
+    $sqlDeleteReviews = "DELETE FROM review WHERE gameID=$gameID";
+    $resultDeleteReviews = mysqli_query($con, $sqlDeleteReviews);
 
-    $result = mysqli_query($con, $sql);
+    if (!$resultDeleteReviews) {
+        die(mysqli_error($con));
+    }
 
+    // Ahora que los registros relacionados en la tabla 'review' han sido eliminados,
+    // procedemos a eliminar el juego de la tabla 'games'
+    $sqlDeleteGame = "DELETE FROM games WHERE gameID=$gameID";
+    $resultDeleteGame = mysqli_query($con, $sqlDeleteGame);
 
-    if ($result) {
+    if ($resultDeleteGame) {
         header('location: games.php');
         die();
     } else {
